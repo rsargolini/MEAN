df47fe135447db7afa6d002fb0fcc952d32695d9
Robert Frenette <robert.frenette@comcast.net>
2019-09-14 00:00:47 -0400

    Updates to file storaage and image names.

--- 2_Express/03_POST_Files/demo/routes/auth.js
+++ 2_Express/03_POST_Files/demo/routes/auth.js
@@ -10,124 +10,136 @@ var user_persist = require('./../utils/user_persist');
 var blog_persist = require('./../utils/blog_persist');
 
 // init Flash
-authRouter.use(flash());  
+authRouter.use(flash());
 
-// Upload Object
-var upload = multer({dest: 'public/uploads/'});
+// Global var to hold username to use as filename prefix
+var fileNamePrefix = '';
+
+// Custom multer storage
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'public/uploads/')
+    },
+    filename: function(req, file, cb) {
+        cb(null, `${fileNamePrefix}_${file.originalname}`)
+    }
+});
+var upload = multer({ storage: storage });
 
 // Register a User
 authRouter.post('/register', (req, res, next) => {
-  var username = req.body.username;
-  var email = req.body.email;
-  var password = req.body.password;
-  log.info(`Register: ${username}, ${email}, ${password}`);
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+    log.info(`Register: ${username}, ${email}, ${password}`);
 
-  var user = user_persist.addUser(username, email, password);
-  if (user) {
-    res.redirect('/users/login');
-  } else {
-    next(new Error('RegistrationFailedError', false));
-  }
+    var user = user_persist.addUser(username, email, password);
+    if (user) {
+        res.redirect('/users/login');
+    } else {
+        next(new Error('RegistrationFailedError', false));
+    }
 });
 
 // User Login
 authRouter.post('/login', (req, res, next) => {
-  var username = req.body.username;
-  var password = req.body.password;
-  log.info(`Login: ${username}, ${password}`);
+    var username = req.body.username;
+    var password = req.body.password;
+    log.info(`Login: ${username}, ${password}`);
 
-  var user = user_persist.authenticate(username, password);
-  if (user) {
-    // put username in session - validate on Blog page
-    req.session.username = username;
-    req.session.blogdata = blog_persist.getBlog(username);
-    res.redirect('/users/blog');
-  } else {
-    next(new Error('LoginFailedError', false));
-  }
+    var user = user_persist.authenticate(username, password);
+    if (user) {
+        // put username in session - validate on Blog page
+        req.session.username = username;
+        req.session.blogdata = blog_persist.getBlog(username);
+        res.redirect('/users/blog');
+    } else {
+        next(new Error('LoginFailedError', false));
+    }
 });
 
 // Blog Entry w/ File Upload
 authRouter.post('/blog', upload.single('imageurl'), (req, res, next) => {
-  // Validate we have username in session - set in login
-  var username = req.session.username;
-  if (username) {
-    var hikename = req.body.hikename;
-    var hikediff = req.body.hikediff;
-    var hikedesc = req.body.hikedesc;
-    var imageurl = '';
-    // check to see if iamge was uploaded
-    try {
-      if (req.file.filename) {
-        imageurl = `/uploads/${req.file.filename}`;
-      }
-    } catch(e) {
-      imageurl = '/img/default.jpg';
-    }
-    log.info(`Blog Post: ${username}, ${hikename}, ${hikediff}, ${hikedesc}, ${imageurl}`);
+    // Validate we have username in session - set in login
+    var username = req.session.username;
+    if (username) {
+        fileNamePrefix = username
+        var hikename = req.body.hikename;
+        var hikediff = req.body.hikediff;
+        var hikedesc = req.body.hikedesc;
+        var imageurl = '';
+        // check to see if iamge was uploaded
+        try {
+            if (req.file.filename) {
+                imageurl = `/uploads/${fileNamePrefix}_${req.file.originalname}`;
+            }
+        } catch (e) {
+            imageurl = '/img/default.jpg';
+        }
+        log.info(`Blog Post: ${username}, ${hikename}, ${hikediff}, ${hikedesc}, ${imageurl}`);
 
-    var blog = blog_persist.addHike(username, {"hikename": hikename, "hikediff": hikediff, "imageurl": imageurl, "hikedesc": hikedesc});
-    if (blog) {
-      req.session.blogdata = blog;
-      res.redirect('/users/blog');
+        var blog = blog_persist.addHike(username, { "hikename": hikename, "hikediff": hikediff, "imageurl": imageurl, "hikedesc": hikedesc });
+        if (blog) {
+            req.session.blogdata = blog;
+            res.redirect('/users/blog');
+        } else {
+            next(new Error('BlogPostFailedError', false));
+        }
     } else {
-      next(new Error('BlogPostFailedError', false));
+        log.error('Error: Attempt to post Blog entry without username.');
+        res.status(500).redirect('/error.html');
     }
-  } else {
-    log.error('Error: Attempt to post Blog entry without username.');
-    res.status(500).redirect('/error.html');
-  }
 });
 
 // Specific Hike
 authRouter.get('/hike/:id', (req, res, next) => {
-  // Validate we have username in session - set in login
-  var username = req.session.username;
-  if (username) {
-    // validate we have a hike id
-    var id = req.params.id;
-    if (! isNaN(parseInt(id))) {
-      var theHike = blog_persist.getHike(username, parseInt(id));
-      res.render('hike.hbs', {
-        pageTitle: 'Hike',
-        hikeData: {
+    // Validate we have username in session - set in login
+    var username = req.session.username;
+    if (username) {
+        // validate we have a hike id
+        var id = req.params.id;
+        if (!isNaN(parseInt(id))) {
+            var theHike = blog_persist.getHike(username, parseInt(id));
+            res.render('hike.hbs', {
+                pageTitle: 'Hike',
+                hikeData: {
                     hikename: theHike[0].hikename,
                     hikediff: theHike[0].hikediff,
                     hikedesc: theHike[0].hikedesc,
                     imageurl: theHike[0].imageurl
-                  },
-        flashMsg: req.flash('HikeDetailsFailedError')
-      });
+                },
+                flashMsg: req.flash('HikeDetailsFailedError')
+            });
+        } else {
+            next(new Error('BlogPostFailedError', false));
+        }
     } else {
-      next(new Error('BlogPostFailedError', false));
+        log.error('Error: Attempt to access Hike without username.');
+        res.status(500).redirect('/error.html');
     }
-  } else {
-    log.error('Error: Attempt to access Hike without username.');
-    res.status(500).redirect('/error.html');
-  }
 });
 
 // Error Route
 authRouter.use((err, req, res, next) => {
-  if (err.message === "RegistrationFailedError") {
-    log.error('RegistrationFailedError: Registration unsuccessful.');
-    req.flash('RegistrationFailedError', 'Registration unsuccessful.');
-    res.redirect('/users/register');
-  } else if (err.message === "LoginFailedError") {
-    log.error('LoginFailedError: Login unsuccessful.');
-    req.flash('LoginFailedError', "Login unsuccessful.");
-    res.redirect('/users/login');
-  } else if (err.message === "BlogPostFailedError") {
-    log.error('BlogPostFailedError: Blog Post unsuccessful.');
-    req.flash('BlogPostFailedError', "Blog Post unsuccessful.");
-    res.redirect('/users/blog');
-  } else if (err.message === "HikeDetailsFailedError") {
-    log.error('HikeDetailsFailedError: HikeDetailsFailedError unsuccessful.');
-    req.flash('HikeDetailsFailedError', 'HikeDetailsFailedError unsuccessful.');
-    res.redirect('/users/blog/');
-  } else {
-    next(err);
-  }
+    if (err.message === "RegistrationFailedError") {
+        log.error('RegistrationFailedError: Registration unsuccessful.');
+        req.flash('RegistrationFailedError', 'Registration unsuccessful.');
+        res.redirect('/users/register');
+    } else if (err.message === "LoginFailedError") {
+        log.error('LoginFailedError: Login unsuccessful.');
+        req.flash('LoginFailedError', "Login unsuccessful.");
+        res.redirect('/users/login');
+    } else if (err.message === "BlogPostFailedError") {
+        log.error('BlogPostFailedError: Blog Post unsuccessful.');
+        req.flash('BlogPostFailedError', "Blog Post unsuccessful.");
+        res.redirect('/users/blog');
+    } else if (err.message === "HikeDetailsFailedError") {
+        log.error('HikeDetailsFailedError: HikeDetailsFailedError unsuccessful.');
+        req.flash('HikeDetailsFailedError', 'HikeDetailsFailedError unsuccessful.');
+        res.redirect('/users/blog/');
+    } else {
+        next(err);
+    }
 });
 
 module.exports = authRouter;

980e7d8daa710d24a9166461e8d9a80fc537d006
Rob Frenette <RMF.HCA2@gmail.com>
2019-09-18 08:54:12 -0400

    Updated

--- 3_MySQL/02_MySQL/demo/services/userService.js
+++ 3_MySQL/02_MySQL/demo/services/userService.js
@@ -4,9 +4,6 @@ var dbConn = require('./../inc/db_connection');
 var dbQuery = require('./../inc/db_queries');
 
 UserService.postUserLogin = (req, res) => {
-    var userName = req.body.user_name;
-    var userPassword = req.body.user_password;
-    var userEmail = req.body.user_email;
     dbConn.getDbConnection(dbQuery.postUserLogin, req, res);
 };
 
